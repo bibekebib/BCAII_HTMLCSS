016e5e7c053bc8196385908372de410e510b6df8
Bibek Chalise <modernbib@gmail.com>
2024-03-20 10:59:41 +0545

    JS Started

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+    //   document.getElementById("main").innerHTML = " <br> Hello, JavaScript!";
+    document.getElementById("main").style.color = "red";
+    document.getElementById("main").style.fontSize = "50px";
+    document.getElementById("main").innerText =4 + 6;
+    console.log(4 + 6), console.log(4 - 6), console.log(4 * 6), console.log(4 / 6);
+    console.log(4 - 6);
+    console.log(4 * 6);
+    console.log(4 / 6);
+    console.log(4 % 3);
+    console.log(4 ** 3);
+    
+
+
+

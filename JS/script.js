d6c443a0552ec3bff0e19b8d7f5eb5f38699280a
Bibek Chalise <modernbib@gmail.com>
2024-03-26 10:13:13 +0545

    datatypes done

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,62 @@
     //   document.getElementById("main").innerHTML = " <br> Hello, JavaScript!";
     document.getElementById("main").style.color = "red";
     document.getElementById("main").style.fontSize = "50px";
-    document.getElementById("main").innerText =4 + 6;
-    console.log(4 + 6), console.log(4 - 6), console.log(4 * 6), console.log(4 / 6);
-    console.log(4 - 6);
-    console.log(4 * 6);
-    console.log(4 / 6);
-    console.log(4 % 3);
-    console.log(4 ** 3);
-    
+    // document.getElementById("main").innerText =4 + 6;
+    // console.log(4 + 6), console.log(4 - 6), console.log(4 * 6), console.log(4 / 6);
+    // console.log(4 - 6);
+    // console.log(4 * 6);
+    // console.log(4 / 6);
+    // console.log(4 % 3);
+    // console.log(4 ** 3);
+
+// var x = 6;
+// let x = 6;
+// const pi = 3.14;
+
+// Number 
+var x = 10;
+console.log(typeof x);
 
 
+// String
+var FName = "John";
+console.log(typeof FName);
 
+// Boolean
+var isAdult = true;
+console.log(typeof isAdult);
+
+// null
+var y = null;
+console.log(typeof y);
+
+// undefined
+var z;
+console.log(typeof z);
+
+// Array
+var cars = ["BMW", "Volvo", "Saab", 'Ford'];
+cars.splice(1, 0, "Audi", "Mustang", "Toyota", "Honda");
+console.log(cars);
+
+console.log(cars);
+
+// indexing in js
+console.log(cars[2]);
+
+cars.push("Audi");
+console.log(cars);
+
+cars.pop();
+console.log(cars);
+
+// console.log(cars.length);
+// console.log('this is it')
+// console.log(cars)
+// console.log(cars.pop([2]));
+// console.log(cars);
+
+// cars.splice(2, 1);
+// console.log(cars);
+
+cars.sp

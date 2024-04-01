80e3792de8422137f119ea717454faaf9cd582c0
Bibek Chalise <modernbib@gmail.com>
2024-04-01 10:59:03 +0545

    dom manipulation added

---
+++ JS/dom.js
@@ -0,0 +1,17 @@
+// document.getElementById('index').innerHTML = 'Hello World!';
+// document.getElementsByClassName('test1')[0].innerHTML = 'Nice to Meet you!';
+
+
+document.getElementsByTagName('p')[0].innerHTML = 'Getting this through p tag';
+
+document.querySelector('.test1').innerHTML = 'This is h1 tag';
+
+document.querySelectorAll('p')[0].classList.remove('test');
+
+
+document.getElementsByClassName('naya-class')[0].style.color = 'red';
+
+
+
+
+

60e1b8250fdd63b678fd3e9fbc57f54aa0f9c731
Bibek Chalise <modernbib@gmail.com>
2024-04-04 10:54:27 +0545

    JS added

---
+++ Project/TODO/script.js
@@ -0,0 +1,59 @@
+const taskInput = document.getElementById("task-input");
+const addButton = document.getElementById("add-button");
+const taskList = document.getElementById("task-list");
+function addTask() {
+    const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const newTask = document.createElement("li");
+    newTask.innerHTML = taskText;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    newTask.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function() {
+      newTask.remove();
+    });
+  }
+}  
+addButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+
+
+
+
+
+
+
+
+// function updateStorage (){
+
+// var list = document.getElementById('task-list').innerHTML
+// localStorage.setItem('todolist', list)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
